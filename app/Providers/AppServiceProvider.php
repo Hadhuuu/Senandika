d7bb23272e307003ac7ab306afadd23b9bb3c61f
Hadhuuu <nandemonaiyahud@gmail.com>
2026-06-10 19:58:11 +0700

    fix : https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // PENTING: Memaksa HTTPS jika aplikasi berjalan di production (Railway)
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
